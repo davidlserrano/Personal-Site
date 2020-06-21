b154bc3dfc73c68d7001b487d2909120a7be5bc3
David Serrano <davidserrano@Davids-MBP-2.attlocal.net>
2020-06-20 22:30:21 -0700

    set up new Vue project and mapped 3 new routes

---
+++ src/views/Hobbies.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hobbies">
+
+    <HelloWorld msg="This is the hobbies Page"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Hobbies',
+  components: {
+    HelloWorld
+  }
+}
+</script>
