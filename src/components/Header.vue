68268608f4cccf8ed9076dbc53f596dfd1b4f6ae
David Serrano <davidserrano@Davids-MBP-2.attlocal.net>
2020-06-24 10:19:10 -0700

    added Menu component and seperated Header from Aop

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div id="nav">
+        <span id="name">David Serrano</span>
+
+        <span id="link"><router-link to="/">Home</router-link></span>
+        <span id="link"><router-link to="/about">About</router-link></span>
+        <span id="link"><router-link to="/projects">Projects</router-link></span>
+        <span id="link"><router-link to="/hobbies">Hobbies</router-link></span>
+        <span id="link"><router-link to="/contact">Contact</router-link></span>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+export default { 
+    name: "Header",
+    components:{
+        
+    }
+}
+</script>
+
+<style>
+    #nav{
+        border-bottom: 1px white solid;
+    }
+
+    #nav a {
+        color: white;
+        text-decoration: none;
+        font-size: 13.5px;
+    }
+     #nav a:hover {
+        font-weight: bolder;
+    }
+
+
+    #nav a.router-link-exact-active {
+      color: #8A897C;
+    }
+    
+    #name{
+        font-family: Didot;
+        color: white;
+        font-weight: 100;
+        font-size: 40px;
+        padding: 0px 25px 0px 20px; 
+    }
+    
+    #link{
+        padding: 0px 3px 0px 3px;
+    }
+
+
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="menu">
+      <div id="options">
+        Option<br>
+        Option<br>
+        Option<br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #options{
+        color: white;
+        width: 105px;
+        height: 150px;
+        margin-top: 10px;
+        margin-left: 25px;
+        border-right: 1px white solid;
+        font-size: 16px;
+    }
+</style>
