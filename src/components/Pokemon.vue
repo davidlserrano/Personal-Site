dae899654533debca3a69a5d743919fc291ea704
David Serrano <davidserrano@Davids-iMac.attlocal.net>
2021-01-09 19:24:17 -0800

    set up layout for the way this project will be displayed

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="display">
-	<text>cock</text>
+
+     <img class="slide" src="@/assets/imgs/pokemon1.png">
+     <img class="slide" src="@/assets/imgs/pokemon2.png">
+     <img class="slide" src="@/assets/imgs/pokemon3.png">
+     <div id ="pdfs">
+        <iframe src="/assets/imgs/pokeReqs1.pdf" type="application/pdf"></iframe>
+        <iframe src="/assets/imgs/pokeReqs2.pdf" type="application/pdf"></iframe>   
+     </div>
+     <img class="slide" src="@/assets/imgs/pokemon4.png">
+     <img class="slide" src="@/assets/imgs/pokemon5.png">
+
    </div>
 </template>
 
@@ -14,12 +24,10 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     .display{
-        display: flex;
-        flex-direction: row;
         overflow: scroll;
         width: inherit;
         height: inherit;
-        background: #353535;
+        background: #212121;
 
     }
 
@@ -38,5 +46,15 @@
         transform: translateX(0px);
     }
 
+    .slide{
+        width: 100%;
+      }
 
+    #pdfs{
+        display: flex;
+        flex-direction: row;
+        overflow: scroll;
+        height: 100%;
+        width: 100%;
+    }
 </style>
